b9ad27870bdbbbd8984fcb937d58235860cc6b43
Phillip DuLion <phillip@dulion.com>
2026-05-28 19:08:49 -0700

    Build GTK Window

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <gtk/gtk.h>
 #include <gst/gst.h>
 
 #define RTSP_RIGHT "rtsp://192.168.16.128:554/stream1"
@@ -12,27 +13,30 @@ typedef struct {
     GstElement *parse;
     GstElement *decode;
     GstElement *convert;
-} RtspData;
+} ChannelData;
 
 typedef struct {
-    RtspData left;
-    RtspData right;
+    GstElement *pipeline;
+    ChannelData left;
+    ChannelData right;
     GstElement *compose;
     GstElement *overlay;
     GstElement *sink;
-    GstElement *pipeline;
+    GtkWidget *sink_widget;
     GMainLoop *loop;
 } StreamData;
 
-static gboolean select_stream_cb(GstElement *src, guint num, const GstCaps *caps, RtspData *data);
+static gboolean pipeline_make(StreamData *data);
 
-static void pad_added_cb(GstElement *src, GstPad *src_pad, const RtspData *data);
+static gboolean channel_make(ChannelData *data, GstElement *pipeline, GstPad *pad, const gchar *uri, const gchar *name);
 
-static GstElement *build_element(const gchar *factory_name, const gchar *prefix, const gchar *suffix);
+static GstElement *element_factory_make(const gchar *factory_name, const gchar *prefix, const gchar *suffix);
 
-static gboolean build_link(GstElement *src, GstElement *sink);
+static gboolean element_link(GstElement *src, GstElement *sink);
 
-static gboolean build_channel(RtspData *data, GstElement *pipeline, GstPad *sink, const gchar *uri, const gchar *name);
+static gboolean select_stream_cb(GstElement *src, guint num, const GstCaps *caps, ChannelData *data);
+
+static void pad_added_cb(GstElement *src, GstPad *src_pad, const ChannelData *data);
 
 static void error_cb(GstBus *bus, GstMessage *msg, const StreamData *data);
 
@@ -40,68 +44,31 @@ GST_DEBUG_CATEGORY_STATIC(rvcam);
 #define GST_CAT_DEFAULT rvcam
 
 int main(int argc, char *argv[]) {
+    GtkWidget *window;
     StreamData data = {0};
-    GstBus *bus;
-    GstPad *sink_pad;
 
+    gtk_init(&argc, &argv);
     gst_init(&argc, &argv);
     GST_DEBUG_CATEGORY_INIT(rvcam, "rvcam", 0, "Log for RV Camera Application");
 
     data.pipeline = gst_pipeline_new("rtsp-client");
-    data.compose = gst_element_factory_make("compositor", "compositor");
-    data.overlay = gst_element_factory_make("clockoverlay", "date-time");
-    data.sink = gst_element_factory_make("autovideosink", "sink");
-    if (!data.pipeline || !data.sink) {
-        GST_ERROR("Not all element could be created.");
-        return -1;
-    }
-    gst_bin_add_many(GST_BIN(data.pipeline), data.compose, data.overlay, data.sink, NULL);
-    if (!gst_element_link(data.compose, data.overlay)) {
-        GST_ERROR("Could not link %s to %s", GST_ELEMENT_NAME(data.compose), GST_ELEMENT_NAME(data.overlay));
-        goto fail;
-    }
-    if (!gst_element_link(data.overlay, data.sink)) {
-        GST_ERROR("Could not link %s to %s", GST_ELEMENT_NAME(data.overlay), GST_ELEMENT_NAME(data.sink));
+    if (!data.pipeline || !pipeline_make(&data)) {
+        GST_ERROR("Could not create rtsp pipeline");
         goto fail;
     }
 
-    sink_pad = gst_element_request_pad_simple(data.compose, "sink_%u");
-    g_object_set(G_OBJECT(sink_pad), "xpos", 0, "ypos", 0, "width", 1920, "height", 1080, NULL);
-    if (!build_channel(&data.left, data.pipeline, sink_pad, RTSP_LEFT, "left_")) {
-        gst_object_unref(sink_pad);
-        goto fail;
-    }
-    gst_object_unref(sink_pad);
-
-    sink_pad = gst_element_request_pad_simple(data.compose, "sink_%u");
-    g_object_set(G_OBJECT(sink_pad), "xpos", 1920, "ypos", 0, "width", 1920, "height", 1080, NULL);
-    if (!build_channel(&data.right, data.pipeline, sink_pad, RTSP_RIGHT, "right_")) {
-        gst_object_unref(sink_pad);
-        goto fail;
-    }
-    gst_object_unref(sink_pad);
-
-    g_object_set(
-        G_OBJECT(data.overlay),
-        "time-format", "%F %T",
-        "font-desc", "Sans 6",
-        "halignment", 1,
-        "valignment", 5,
-        "ypos", 0.0,
-        NULL);
-
-    bus = gst_element_get_bus(data.pipeline);
-    gst_bus_add_signal_watch(bus);
-    g_signal_connect(G_OBJECT(bus), "message::error", G_CALLBACK(error_cb), NULL);
-    gst_object_unref(bus);
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_container_add(GTK_CONTAINER(window), data.sink_widget);
+    gtk_window_fullscreen(GTK_WINDOW(window));
+    gtk_widget_show_all(window);
 
     if (gst_element_set_state(data.pipeline, GST_STATE_PLAYING) == GST_STATE_CHANGE_FAILURE) {
         GST_ERROR("Unable to set the pipeline to the playing state.");
         goto fail;
     }
 
-    data.loop = g_main_loop_new(NULL, FALSE);
-    g_main_loop_run(data.loop);
+    gtk_main();
 
     gst_element_set_state(data.pipeline, GST_STATE_NULL);
     gst_object_unref(data.pipeline);
@@ -112,19 +79,84 @@ fail:
     return -1;
 }
 
-static gboolean build_channel(
-    RtspData *data,
+static gboolean pipeline_make(StreamData *data) {
+    GstBus *bus;
+    GstPad *sink_pad;
+    GstElement *gl_sink;
+
+    gl_sink = element_factory_make("gtkglsink", "main_", "gtkglsink");
+    if (!gl_sink) {
+        data->sink = element_factory_make("gtksink", "main_", "gtksink");
+        if (!data->sink) {
+            return FALSE;
+        }
+        g_object_get(data->sink, "widget", &data->sink_widget, NULL);
+    } else {
+        data->sink = element_factory_make("glsinkbin", "main_", "glsinkbin");
+        if (!data->sink) {
+            return FALSE;
+        }
+        g_object_set(data->sink, "sink", gl_sink, NULL);
+        g_object_get(gl_sink, "widget", &data->sink_widget, NULL);
+    }
+
+    data->compose = element_factory_make("compositor", "main_", "compositor");
+    data->overlay = element_factory_make("clockoverlay", "main_", "date-time");
+    if (!data->compose || !data->overlay || !data->sink) {
+        return FALSE;
+    }
+    gst_bin_add_many(GST_BIN(data->pipeline), data->compose, data->overlay, data->sink, NULL);
+    if (!element_link(data->compose, data->overlay) ||
+        !element_link(data->overlay, data->sink)) {
+        return FALSE;
+    }
+
+    sink_pad = gst_element_request_pad_simple(data->compose, "sink_%u");
+    g_object_set(G_OBJECT(sink_pad), "xpos", 0, "ypos", 0, "width", 1920, "height", 1080, NULL);
+    if (!channel_make(&data->left, data->pipeline, sink_pad, RTSP_LEFT, "left_")) {
+        gst_object_unref(sink_pad);
+        return FALSE;
+    }
+    gst_object_unref(sink_pad);
+
+    sink_pad = gst_element_request_pad_simple(data->compose, "sink_%u");
+    g_object_set(G_OBJECT(sink_pad), "xpos", 1920, "ypos", 0, "width", 1920, "height", 1080, NULL);
+    if (!channel_make(&data->right, data->pipeline, sink_pad, RTSP_RIGHT, "right_")) {
+        gst_object_unref(sink_pad);
+        return FALSE;
+    }
+    gst_object_unref(sink_pad);
+
+    g_object_set(
+        G_OBJECT(data->overlay),
+        "time-format", "%F %T",
+        "font-desc", "Sans 6",
+        "halignment", 1,
+        "valignment", 5,
+        "ypos", 0.0,
+        NULL);
+
+    bus = gst_element_get_bus(data->pipeline);
+    gst_bus_add_signal_watch(bus);
+    g_signal_connect(G_OBJECT(bus), "message::error", G_CALLBACK(error_cb), NULL);
+    gst_object_unref(bus);
+
+    return TRUE;
+}
+
+static gboolean channel_make(
+    ChannelData *data,
     GstElement *pipeline,
-    GstPad *sink,
+    GstPad *pad,
     const gchar *uri,
     const gchar *name) {
     g_autoptr(GstPad) src_pad = NULL;
 
-    data->rtsp = build_element("rtspsrc", name, "rtsp");
-    data->depay = build_element("rtph265depay", name, "depay");
-    data->parse = build_element("h265parse", name, "parse");
-    data->decode = build_element(RTSP_DECODER, name, "decode");
-    data->convert = build_element("videoconvert", name, "convert");
+    data->rtsp = element_factory_make("rtspsrc", name, "rtsp");
+    data->depay = element_factory_make("rtph265depay", name, "depay");
+    data->parse = element_factory_make("h265parse", name, "parse");
+    data->decode = element_factory_make(RTSP_DECODER, name, "decode");
+    data->convert = element_factory_make("videoconvert", name, "convert");
     if (!data->rtsp || !data->depay || !data->parse || !data->decode || !data->convert) {
         GST_ERROR("Not all rtsp element could be created.");
         return FALSE;
@@ -139,15 +171,15 @@ static gboolean build_channel(
         data->convert,
         NULL);
 
-    if (!build_link(data->depay, data->parse) ||
-        !build_link(data->parse, data->decode) ||
-        !build_link(data->decode, data->convert)) {
+    if (!element_link(data->depay, data->parse) ||
+        !element_link(data->parse, data->decode) ||
+        !element_link(data->decode, data->convert)) {
         GST_ERROR("Could not link elements for channel");
         return FALSE;
     }
 
     src_pad = gst_element_get_static_pad(data->convert, "src");
-    if (GST_PAD_LINK_FAILED(gst_pad_link(src_pad, sink))) {
+    if (GST_PAD_LINK_FAILED(gst_pad_link(src_pad, pad))) {
         GST_INFO("Could not link convert element to compositor.");
         return FALSE;
     }
@@ -158,7 +190,7 @@ static gboolean build_channel(
     return TRUE;
 }
 
-static GstElement *build_element(const gchar *factory_name, const gchar *prefix, const gchar *suffix) {
+static GstElement *element_factory_make(const gchar *factory_name, const gchar *prefix, const gchar *suffix) {
     g_autofree const gchar *element_name = g_strconcat(prefix, suffix, NULL);
     GstElement *element = gst_element_factory_make(factory_name, element_name);
     if (!element) {
@@ -167,7 +199,7 @@ static GstElement *build_element(const gchar *factory_name, const gchar *prefix,
     return element;
 }
 
-static gboolean build_link(GstElement *src, GstElement *sink) {
+static gboolean element_link(GstElement *src, GstElement *sink) {
     if (!gst_element_link(src, sink)) {
         GST_ERROR("Could not link %s to %s", GST_ELEMENT_NAME(src), GST_ELEMENT_NAME(sink));
         return FALSE;
@@ -175,10 +207,10 @@ static gboolean build_link(GstElement *src, GstElement *sink) {
     return TRUE;
 }
 
-static gboolean select_stream_cb(GstElement *src, guint num, const GstCaps *caps, RtspData *data) {
+static gboolean select_stream_cb(GstElement *src, guint num, const GstCaps *caps, ChannelData *data) {
     const gchar *media, *encoding;
 
-     GstStructure *structure = gst_caps_get_structure(caps, 0);
+    GstStructure *structure = gst_caps_get_structure(caps, 0);
     media = gst_structure_get_string(structure, "media");
     encoding = gst_structure_get_string(structure, "encoding-name");
     if (media && encoding && strcmp(media, "video") == 0 && strcmp(media, "H265") != 0) {
@@ -187,7 +219,7 @@ static gboolean select_stream_cb(GstElement *src, guint num, const GstCaps *caps
     return FALSE;
 }
 
-static void pad_added_cb(GstElement *src, GstPad *src_pad, const RtspData *data) {
+static void pad_added_cb(GstElement *src, GstPad *src_pad, const ChannelData *data) {
     g_autoptr(GstCaps) src_caps = NULL;
     g_autoptr(GstPad) sink_pad = NULL;
     GstStructure *caps_struct = NULL;
@@ -201,15 +233,15 @@ static void pad_added_cb(GstElement *src, GstPad *src_pad, const RtspData *data)
     src_type = gst_structure_get_name(caps_struct);
     caps = gst_caps_to_string(src_caps);
     GST_INFO("RTSP pad '%s' type '%s' has %d capabilities: %s",
-            src_name,
-            src_type,
-            gst_caps_get_size(src_caps),
-            caps);
+             src_name,
+             src_type,
+             gst_caps_get_size(src_caps),
+             caps);
 
     sink_pad = gst_element_get_static_pad(data->depay, "sink");
     if (gst_pad_is_linked(sink_pad)) {
         GST_ERROR(
-            "Source pad '%s' linked to '%s' pad '%s'.",
+            "RTSP pad '%s' linked to '%s' pad '%s'.",
             src_name,
             GST_ELEMENT_NAME(data->depay),
             GST_PAD_NAME(sink_pad));
@@ -220,7 +252,7 @@ static void pad_added_cb(GstElement *src, GstPad *src_pad, const RtspData *data)
         GST_INFO("RTSP pad '%s' type '%s' could not be linked.", src_name, src_type);
     } else {
         GST_INFO(
-            "Source pad '%s' type '%s' linked to '%s' pad '%s'.",
+            "RTSP pad '%s' type '%s' linked to '%s' pad '%s'.",
             src_name,
             src_type,
             GST_ELEMENT_NAME(data->depay),
